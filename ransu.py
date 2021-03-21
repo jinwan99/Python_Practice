bb11c2d8e5a444155f64f475626458c7fc3ca189
Leejinwan <leejinwan99@gmail.com>
2021-03-21 21:02:21 +0900

    [update] randint, randrange

--- ransu.py
+++ ransu.py
@@ -6,3 +6,13 @@ print(int(random() * 10))
 print(int(random() * 10))
 print(int(random() * 10))
 print(int(random() * 10) + 1)
+
+print(int(random() * 45) +1)
+print(int(random() * 45) +1)
+print(int(random() * 45) +1)
+print(int(random() * 45) +1)
+print(int(random() * 45) +1)
+print(int(random() * 45) +1)
+
+print(randrange(1,46)) # 1~46 미만의 임의의 값을 생성
+print(randint(1,46))
