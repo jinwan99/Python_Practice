e261ff5d6493ff3d15278a84ab885de426eca820
Leejinwan <leejinwan99@gmail.com>
2021-03-21 20:43:48 +0900

    random乱数_練習

---
+++ ransu.py
@@ -0,0 +1,8 @@
+from random import *
+
+print(random()) # 0.0 ~ 1.0 미만의 임의의 값을 생성
+print(random() * 10)
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10) + 1)
