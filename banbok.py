106aa740e311f94696ffcec83f6bf874fda652e6
jinwan99 <49511023+jinwan99@users.noreply.github.com>
2021-04-19 01:27:19 +0900

    2021년 4월 19일 for문 연습
    for - practice

---
+++ banbok.py
@@ -0,0 +1,17 @@
+# print("대기번호:1")
+# print("대기번호:2")
+# print("대기번호:3")
+# print("대기번호:4")
+
+# for waiting_no in [0,1,2,3,4]:
+#     print("대기번호 : {0}".format(waiting_no))
+#
+# for waiting_no in range(5): # 0,1,2,3,4랑 똑같음
+#     print(("대기번호 : {0}".format(waiting_no)))
+
+# for waiting_no in range(1,6): # 1,2,3,4,5가 나옴
+#     print(("대기번호 : {0}".format(waiting_no)))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print(f"{customer}, 커피가 준비되었습니다.")
